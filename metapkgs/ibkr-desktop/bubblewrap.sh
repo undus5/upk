7ed2bf345309fcfa69f35604f27f777bcd1e83e7
undus5 <mail@undus.net>
2026-06-08 17:22:29 +0800

    add ibkr-desktop package

---
+++ metapkgs/ibkr-desktop/bubblewrap.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+sandbox_dir="${1}"
+exec_path="${2}"
+home_dir=$(realpath ~)
+
+errf() { printf "${@}\n" >&2; exit 1; }
+get_help() { echo "Usage: $(basename $0) <sandbox_dir> <exec_path>"; }
+
+[[ -z "$sandbox_dir" || -z "$exec_path" ]] && get_help && exit 1
+[[ -d "$sandbox_dir" ]] || errf "directory not found: ${sandbox_dir}"
+
+binds_or_links=
+if [[ -L /bin ]]; then
+    binds_or_links+=" --symlink $(realpath /bin) /bin"
+else
+    binds_or_links+=" --ro-bind /bin /bin"
+fi
+if [[ -L /lib ]]; then
+    binds_or_links+=" --symlink $(realpath /lib) /lib"
+else
+    binds_or_links+=" --ro-bind /lib /lib"
+fi
+if [[ -L /lib64 ]]; then
+    binds_or_links+=" --symlink $(realpath /lib64) /lib64"
+else
+    binds_or_links+=" --ro-bind /lib64 /lib64"
+fi
+
+bwrap \
+  --ro-bind /etc /etc \
+  --ro-bind /sys /sys \
+  --ro-bind /usr /usr \
+  --dev /dev \
+  --dev-bind /dev/dri /dev/dri \
+  --proc /proc \
+  --tmpfs /tmp \
+  --unshare-all \
+  --share-net \
+  --bind $sandbox_dir $home_dir \
+  $binds_or_links $exec_path
+
---
+++ metapkgs/ibkr-desktop/ibkr-desktop.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+upk_src=$(dirname $(realpath $(which upk)))
+source ${upk_src}/metapkg-pre.in
+
+metapkg_dir=$(dirname $(realpath ${BASH_SOURCE[0]}))
+pkg_id=$(basename $metapkg_dir)
+cache_old=${cache_dir}/${pkg_id}.old
+
+installed_dir=${apps_dir}/${pkg_id}
+
+install_pkg() {
+    test_var cache_old $cache_old
+    test_var installed_dir $installed_dir
+    local local_ver=$(get_local_ver)
+    [[ "$local_ver" == "locked" ]] && exit 0
+    local filename="ntws-latest-standalone-linux-x64.sh"
+    local dl_url="https://download2.interactivebrokers.com/installers/ntws"
+    dl_url+="/latest-standalone/${filename}"
+    dl_file=${cache_dir}/${filename}
+    echo "==> downloading ${filename} ..."
+    if [[ -f "${dl_file}" ]]; then
+        echo "==> found in cache"
+    else
+        curl --create-dirs -o ${dl_file} -#L ${dl_url}
+    fi
+    # backup old installed
+    [[ -d $cache_old ]] && rm -rf $cache_old
+    [[ -d $installed_dir ]] && mv $installed_dir $cache_old
+    # backup end
+    unpack_dir=${cache_dir}/${pkg_id}
+    mkdir -p $unpack_dir
+    chmod u+x $dl_file
+    cp $dl_file ${unpack_dir}/
+    ${metapkg_dir}/bubblewrap.sh $unpack_dir ~/${filename}
+    rm ${unpack_dir}/${filename}
+    mv ${unpack_dir} ${installed_dir}
+    echo "==> installed '$(tilde_path $installed_dir)'"
+    lock_ver
+}
+
+launch_pkg() {
+    test_var metapkg_dir $metapkg_dir
+    test_var installed_dir $installed_dir
+    ${metapkg_dir}/bubblewrap.sh $installed_dir ~/ntws/ntws
+}
+
+source ${upk_src}/metapkg-post.in
+
