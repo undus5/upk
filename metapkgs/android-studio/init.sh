88a24c11c66aeaf298e80c7dfc377eb43952bedd
undus5 <mail@undus.net>
2026-07-23 14:55:25 +0800

    add openrgb

--- metapkgs/android-studio/init.sh
+++ metapkgs/android-studio/init.sh
@@ -3,13 +3,8 @@
 exec_path=${installed_dir}/bin/studio
 
 install_pkg() {
-   test_var pkg_id $pkg_id
    local local_ver=$(get_local_ver)
-   if [[ "$local_ver" == "locked" ]]; then
-      echo "==> '$pkg_id' already updated"
-      exit 0   # prevent enable_entry(), post_enable()
-   fi
-   printf "==> ${pkg_id} not support auto installation  \n"
+   printf "==> ${pkg_id} not support auto installation\n"
    printf "==> 1. download from: https://developer.android.com/studio\n"
    printf "==> 2. put into '$(tilde_path $installed_dir)'\n"
    printf "==> 3. enable desktop entry\n"
---
+++ metapkgs/openrgb/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+exec_path="${installed_dir}/openrgb.AppImage"
+
+install_pkg() {
+   local local_ver=$(get_local_ver)
+   printf "==> ${pkg_id} releases do not follow consistent pattern"
+   printf ", install manually\n"
+   printf "==> 1. download AppImage from:"
+   printf " https://github.com/CalcProgrammer1/OpenRGB/releases/\n"
+   printf "==> 2. put into '$(tilde_path ${exec_path})'\n"
+   printf "==> 3. enable desktop entry\n"
+   printf "==> 4. lock package (to mark package as installed)\n"
+   printf "==> 5. download '60-openrgb.rules', put into '/etc/udev/rules.d/'\n"
+   printf "==> 6. run 'udevadm control --reload-rules' and 'udevadm trigger'\n"
+   exit 0   # prevent enable_entry(), post_enable()
+}
