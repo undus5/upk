f1d2d6305b2a208a4f5e0c334e24e08eb766f975
undus5 <mail@undus.net>
2026-07-30 07:41:19 +0800

    fix brave-origin

--- metapkgs/brave-origin/init.sh
+++ metapkgs/brave-origin/init.sh
@@ -25,7 +25,7 @@ install_pkg() {
    local api_url="https://api.github.com/repos/brave/brave-browser"
    api_url+="/releases/tags/v${remote_ver}"
    local filename="${pkg_id}-${remote_ver}-linux-amd64.zip"
-   local dl_url=$(curl -sL $api_url | jq -r "$(jq_dl_url_filter $filename)")
+   local dl_url=$(curl -sL $api_url | jq -r "$(jq_dl_filter $filename)")
    local save_path=${cache_dir}/${filename}
 
    download_file $dl_url $save_path
